799403be9088d8c0f605a6a603bdce4bd0528e86
Kazanin Roman <kazanin-roman@mail.ru>
2025-01-18 20:12:03 +0600

    Add Empty AngularApp

---
+++ orders-app/.angular/cache/19.1.2/orders-app/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,36 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  setAngularAppEngineManifest,
+  setAngularAppManifest
+} from "./chunk-55M7GNG5.js";
+import "./chunk-SNPIQTWQ.js";
+import "./chunk-JMKWHP4H.js";
+import "./chunk-KQVBZON6.js";
+import "./chunk-FCQEZCE4.js";
+import "./chunk-B4CBNP6H.js";
+import "./chunk-2EWNKAHN.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
