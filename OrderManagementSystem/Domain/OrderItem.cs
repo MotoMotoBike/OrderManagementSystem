ef2481c62935cd1abc1f394521ad3d07492b9e2e
Kazanin Roman <kazanin-roman@mail.ru>
2025-01-18 15:15:39 +0600

    Edit Id Type

--- OrderManagementSystem/Domain/OrderItem.cs
+++ OrderManagementSystem/Domain/OrderItem.cs
@@ -11,6 +11,6 @@ public class OrderItem : IEntity
     public string? ProductName { get; set; }
     public int Quantity { get; set; }
     public decimal UnitPrice { get; set; }
-    public int OrderId { get; set; }
+    public long OrderId { get; set; }
     public Order? Order { get; set; }
 }
