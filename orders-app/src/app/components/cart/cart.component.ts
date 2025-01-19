816e958642620480c93c417b72c48342f196bb35
Kazanin Roman <kazanin-roman@mail.ru>
2025-01-19 16:01:00 +0600

    Add complete order action

--- orders-app/src/app/components/cart/cart.component.ts
+++ orders-app/src/app/components/cart/cart.component.ts
@@ -1,17 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {CartItem} from '../../services/cart.item';
 import {CartService} from '../../services/cart.service';
 import {CommonModule} from '@angular/common';
-
+import {environment} from '../../../environments/environment.development';
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  imports: [CommonModule],
+  imports: [HttpClientModule,CommonModule],
 })
 export class CartComponent implements OnInit {
   cartItems: CartItem[] = [];
 
-  constructor(private cartService: CartService) {}
+  serverUrl: string = environment.apiBaseUrl;
+  constructor(
+    private http: HttpClient,
+    private cartService: CartService) {}
 
   ngOnInit(): void {
     this.loadCart();
@@ -37,10 +42,28 @@ export class CartComponent implements OnInit {
   calculateTotalPrice(): number {
     return this.cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
   }
+
+  mapCartToOrderDto(cartItems: CartItem[]): OrderItemDto[] {
+    return cartItems.map(item => ({
+      itemId: item.id,       // Используем id из CartItem как itemId
+      quantity: item.quantity  // Количество товара остаётся таким же
+    }));
+  }
+
+
   checkout(): void {
     if (this.cartItems.length === 0) {
       alert("Ваша корзина пуста. Добавьте товары перед оформлением заказа.");
     } else {
+
+      let orderDto: OrderItemDto[] = this.mapCartToOrderDto(this.cartItems);
+
+      this.http.post(this.serverUrl + 'Orders/CreateOrder',
+        { itemIds: orderDto})
+        .subscribe((response) => {
+          console.log(response);
+        });
+
       // Логика оформления заказа (например, отправка данных на сервер или другое действие)
       alert("Заказ оформлен! Спасибо за покупку.");
       this.clearCart(); // Очистка корзины после оформления
@@ -52,4 +75,12 @@ export class CartComponent implements OnInit {
     this.cartService.clearCart();
     this.loadCart();
   }
+}
+export interface OrderItemDto {
+  itemId: number;
+  quantity: number;
+}
+
+export interface CreateOrderDto {
+  itemIds: OrderItemDto[];
 }
